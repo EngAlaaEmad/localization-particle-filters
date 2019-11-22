ef9fdb97c623aebd6631c026694336747f209426
adam-ubuntu <gy.adam90@gmail.com>
2019-11-22 15:05:16 -0800

    Add initialization code to ParticleFilter::init

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -30,7 +30,24 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
    * NOTE: Consult particle_filter.h for more information about this method 
    *   (and others in this file).
    */
-  num_particles = 0;  // TODO: Set the number of particles
+  num_particles = 20;  // TODO: Set the number of particles
+
+  // Create normal distributions from initial location estimate
+  std::normal_distribution<double> dist_x(x, std[0]);
+  std::normal_distribution<double> dist_y(y, std[1]);
+  std::normal_distribution<double> dist_theta(theta, std[2]);
+
+  std::default_random_engine gen;
+
+  // Initialize particles
+  for (int i = 0; i < num_particles; i++){
+    Particle new_particle;
+    new_particle.x = dist_x(gen);
+    new_particle.y = dist_y(gen);
+    new_particle.theta = dist_theta(gen);
+    new_particle.weight = 1.0;
+    particles.push_back(new_particle);
+  }
 
 }
 
